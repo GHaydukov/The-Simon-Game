2569cfdd75ac03a67a72cffb3908980c621b87ba
unknown <georgi.haydukov99@gmail.com>
2022-10-09 19:09:39 +0200

    Changed REstart to Start

--- javascript/game.js
+++ javascript/game.js
@@ -69,7 +69,7 @@ function checkAnswer(currentLevel) {
       $("body").removeClass("game-over");
     }, 200);
 
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("#level-title").text("Game Over, Press Any Key to Start");
     startOver();
   }
 }
